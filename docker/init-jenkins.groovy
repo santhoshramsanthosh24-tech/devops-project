e58dd2d4f9e74cf6bb1b9a8f18c5b0f7c3af57c0
Santhosh Ram <santhoshramsanthosh24-tech>
2026-04-25 11:52:58 +0000

    fix:the yaml file error

--- docker/init-jenkins.groovy
+++ docker/init-jenkins.groovy
@@ -1,26 +1,45 @@
 import jenkins.model.Jenkins
 import hudson.security.SecurityRealm
-import hudson.security.AuthorizationStrategy
-import jenkins.security.s2m.AdminWhitelistRule
+import hudson.security.FullControlOnceLoggedInAuthorizationStrategy
+import hudson.security.HudsonPrivateSecurityRealm
 
-// Disable setup wizard
-System.setProperty("jenkins.install.runSetupWizard", "false")
-
-// Get Jenkins instance
 def jenkins = Jenkins.getInstance()
 
-// Disable CSRF protection for simplified setup
-jenkins.setCrumbIssuer(null)
+// Disable setup wizard
+System.setProperty("hudson.model.UpdateCenter.never", "true")
 
-// Allow script approval
-jenkins.getExtensionList('hudson.security.csrf.CrumbIssuer').each {
-    it.getDescriptor()?.store()
+// Get or create the security realm
+def realm = jenkins.getSecurityRealm()
+if (realm instanceof HudsonPrivateSecurityRealm) {
+    def adminUser = realm.getUser("admin")
+    if (adminUser != null) {
+        // Update existing admin password
+        adminUser.setPassword("Admin@123456")
+        realm.saveUser(adminUser)
+    }
+} else {
+    // Create new HudsonPrivateSecurityRealm if it doesn't exist
+    realm = new HudsonPrivateSecurityRealm(true, false)
+    jenkins.setSecurityRealm(realm)
+    
+    // Create admin user
+    def adminUser = realm.createAccount("admin", "Admin@123456")
+    realm.saveUser(adminUser)
 }
 
-// Set executor count
+// Set authorization strategy
+def authStrategy = new FullControlOnceLoggedInAuthorizationStrategy()
+jenkins.setAuthorizationStrategy(authStrategy)
+
+// Set number of executors
 jenkins.setNumExecutors(2)
+
+// Disable CSRF protection
+jenkins.setCrumbIssuer(null)
 
 // Save configuration
 jenkins.save()
 
-println("Jenkins initialized - setup wizard disabled")
+println("Jenkins initialized successfully")
+println("Admin user configured: admin")
+
